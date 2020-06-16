21499e24f5e410ace1d728e680d542aa07adc308
Adrian Gonciarz <adrian.gonciarz@appdate.pl>
2020-06-16 19:03:32 +0200

    fixes

--- 3/src/test/test_api.py
+++ 3/src/test/test_api.py
@@ -2,17 +2,17 @@ import requests
 import random
 import os
 
-base_uri = os.getenv('API_HOST', http://localhost)
+base_uri = os.getenv('API_HOST', 'http://localhost')
 port = os.getenv('API_PORT',3000)
 posts_uri = f'{base_uri}:{port}/posts'
 
 
 def test_api_get_posts():
-	r = request.get(posts_uri)
+	r = requests.get(posts_uri)
 	assert r.status_code == 200
 
 def test_creating_post():
-	marker = random.randint(100000)
-	body = {'title': 'test post', 'author': 'ag'}
-	r = request.post(posts_uri, json=body)
-	assert r.status_code == 200
+	marker = random.randint(1, 100000)
+	body = {'title': f'test post{marker}', 'author': 'ag'}
+	r = requests.post(posts_uri, json=body)
+	assert r.status_code == 201
